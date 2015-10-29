f7045406ecaed71aecc42f1311c26f6c8713b1ae
gfatori <gabriel.fatori@gmail.com>
2015-10-29 21:33:39 -0200

    started the project, creating tables...

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,23 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE IF NOT EXISTS player (
+	id INTEGER PRIMARY KEY ASC, name TEXT);
+
+CREATE TABLE IF NOT EXISTS tournament (
+	id INTEGER PRIMARY KEY ASC, 
+	name TEXT,
+	player INTEGER references player (id)
+	champion INTEGER references player (id)
+	);
+
+CREATE TABLE IF NOT EXISTS match (
+	id INTEGER PRIMARY KEY ASC,
+	tournament INTEGER references tournament (id), 
+	player_one INTEGER references player (id),
+	player_two INTEGER references player (id),
+	score_player_one INTEGER,
+	score_player_two INTEGER,
+	name TEXT,
+	);
 
