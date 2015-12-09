f123519038408953ef4d4d3fd53df7785ad5b2b3
gfatori <gabriel.fatori@gmail.com>
2015-12-09 21:01:04 -0200

    Added functionalities to tournament.py, remodeled the sql and pep8 changes to test and other modules.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,32 +13,53 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("delete from match;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("delete from player;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("select count(*) from player;")
+    players_count = c.fetchall()
+    conn.close()
+    return players_count
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("insert into player (name) values ('{}');".format(name))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -56,16 +77,30 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    c = conn.cursor()
+    c.execute(
+        "insert into match (winner, loser)"
+        "select "
+        "p1.id, p2.id"
+        "from player p1 "
+        "cross join player p2"
+        "where"
+        "p1.name = '{0}'"
+        "and"
+        "p2.name = '{1}';".format(winner, loser))
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +108,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,28 +1,9 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-
 CREATE TABLE IF NOT EXISTS player (
-	id INTEGER PRIMARY KEY ASC, name TEXT);
-
-CREATE TABLE IF NOT EXISTS tournament (
-	id INTEGER PRIMARY KEY ASC, 
-	name TEXT,
-	player INTEGER references player (id)
-	champion INTEGER references player (id)
-	);
+	id SERIAL PRIMARY KEY, 
+	name TEXT);
 
 CREATE TABLE IF NOT EXISTS match (
-	id INTEGER PRIMARY KEY ASC,
-	tournament INTEGER references tournament (id), 
-	player_one INTEGER references player (id),
-	player_two INTEGER references player (id),
-	score_player_one INTEGER,
-	score_player_two INTEGER,
-	name TEXT,
+	id SERIAL PRIMARY KEY,
+	winner INTEGER references player (id),
+	loser INTEGER references player (id)
 	);
-
